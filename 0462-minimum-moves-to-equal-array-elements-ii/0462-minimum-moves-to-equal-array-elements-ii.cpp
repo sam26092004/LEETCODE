7140b87390119fc8817592905d3b9bee39370b09
sam26092004 <231158@iiitt.ac.in>
2025-03-08 22:26:37 +0530

    Time: 1 ms (39.76%), Space: 15.7 MB (78.84%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    int minMoves2(vector<int>& nums) {
-        int n = nums.size();
+   int minMoves2(vector<int>& nums) {
         sort(nums.begin(), nums.end()); 
-
-        vector<int> suffixSum(n + 1, 0);
-        int prefixSum = 0;
+        int median = nums[nums.size() / 2]; 
+        int cost = 0;
 
       
-        for (int i = n - 1; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + nums[i];
-        }
-
-      long long cost = LLONG_MAX;
-        for (int i = 0; i < n; i++) {
-        long long prevCost = (i == 0) ? 0 : (1LL*nums[i] * i) - prefixSum;
-        long long nextCost = ((suffixSum[i + 1]) - (1LL*(n - i - 1) * nums[i]));
-            cost = min(cost, prevCost + nextCost);
-
-            prefixSum += nums[i]; 
+        for (int num : nums) {
+            cost += abs(num - median);
         }
 
         return cost;
