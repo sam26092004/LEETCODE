9f5b2ad94fd4717f0b3406141f0ce29493b3bfae
sam26092004 <231158@iiitt.ac.in>
2025-03-24 18:54:11 +0530

    Time: 14 ms (38.31%), Space: 38 MB (36.7%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,71 +1,47 @@
-#define ll long long int
-const int MOD = 1e9 + 7;
-
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        ll dp[n][n][2];
-        // dp[src][dest][0] stores the minimum time between src and dest
-        // dp[src][dest][1] stores the number of ways to reach dest from src
-        // with the minimum time
 
-        // Initialize the dp table
-        for (ll src = 0; src < n; src++) {
-            for (ll dest = 0; dest < n; dest++) {
-                if (src != dest) {
-                    // Set a large initial time
-                    dp[src][dest][0] = 1e12;
-                    // No paths yet
-                    dp[src][dest][1] = 0;
-                } else {
-                    // Distance from a node to itself is 0
-                    dp[src][dest][0] = 0;
-                    // Only one trivial way (staying at the node)
-                    dp[src][dest][1] = 1;
+        vector<list<pair<int, int>>> edges(n);
+        for (const auto& road : roads) {
+            int u = road[0], v = road[1], time = road[2];
+            edges[u].push_back({v, time});
+            edges[v].push_back({u, time});
+        }
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+                       greater<pair<long long, int>>>
+            pq;
+        vector<long long> distances(n, LLONG_MAX);
+        vector<long long> ways(n, 0);
+        const int mod = 1e9 + 7;
+
+        distances[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            auto [currDistance, node] = pq.top();
+            pq.pop();
+
+            if (currDistance > distances[node])
+                continue;
+
+            for (const auto& [nextNode, travelTime] : edges[node]) {
+                long long newDist = currDistance + (long long)travelTime;
+
+                if (newDist < distances[nextNode]) {
+                    distances[nextNode] = newDist;
+                    pq.push({newDist, nextNode});
+                    ways[nextNode] = ways[node];
+                }
+
+                else if (newDist == distances[nextNode]) {
+                    ways[nextNode] = (ways[nextNode] + ways[node]) % mod;
                 }
             }
         }
 
-        // Initialize direct roads from the input
-        for (auto& road : roads) {
-            ll startNode = road[0], endNode = road[1], travelTime = road[2];
-            dp[startNode][endNode][0] = travelTime;
-            dp[endNode][startNode][0] = travelTime;
-            // There is one direct path
-            dp[startNode][endNode][1] = 1;
-            // Since the roads are bidirectional
-            dp[endNode][startNode][1] = 1;
-        }
-
-        // Apply the Floyd-Warshall algorithm to compute shortest paths
-        // Intermediate node
-        for (ll mid = 0; mid < n; mid++) {
-            // Starting node
-            for (ll src = 0; src < n; src++) {
-                // Destination node
-                for (ll dest = 0; dest < n; dest++) {
-                    // Avoid self-loops
-                    if (src != mid && dest != mid) {
-                        ll newTime = dp[src][mid][0] + dp[mid][dest][0];
-
-                        if (newTime < dp[src][dest][0]) {
-                            // Found a shorter path
-                            dp[src][dest][0] = newTime;
-                            dp[src][dest][1] =
-                                (dp[src][mid][1] * dp[mid][dest][1]) % MOD;
-                        } else if (newTime == dp[src][dest][0]) {
-                            // Another way to achieve the same shortest time
-                            dp[src][dest][1] =
-                                (dp[src][dest][1] +
-                                 dp[src][mid][1] * dp[mid][dest][1]) %
-                                MOD;
-                        }
-                    }
-                }
-            }
-        }
-
-        // Return the number of shortest paths from node (n-1) to node 0
-        return dp[n - 1][0][1];
+        return ways[n - 1] % mod;
     }
 };
