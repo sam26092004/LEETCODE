df742a5f198bc8e4f44f2959a3bbf05de7f13899
sam26092004 <231158@iiitt.ac.in>
2025-03-25 20:29:50 +0530

    Time: 95 ms (59.1%), Space: 105.1 MB (62.16%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    void topoSort(unordered_map<int, list<int>>& adjList, vector<int>& visited,
+                  stack<int>& st, int node) {
+        visited[node] = 1;
+        for (auto const& neighbor : adjList[node]) {
+            if (!visited[neighbor]) {
+                topoSort(adjList, visited, st, neighbor);
+            }
+        }
+        st.push(node);
+    }
+
+    vector<string> findAllRecipes(vector<string>& recipes,
+                                  vector<vector<string>>& ingredients,
+                                  vector<string>& supplies) {
+        vector<string> ans;
+        unordered_map<string, int> isPresent;
+        unordered_map<string, bool> isAvailable;
+
+        for (auto const& supply : supplies) {
+            isAvailable[supply] = true;
+        }
+
+        unordered_map<int, list<int>> adjList;
+
+        for (int i = 0; i < recipes.size(); i++) {
+            isPresent[recipes[i]] = i;
+        }
+
+        for (int i = 0; i < recipes.size(); i++) {
+            for (const string& ingredient : ingredients[i]) {
+                if (isPresent.find(ingredient) != isPresent.end()) {
+                    adjList[isPresent[ingredient]].push_back(i);
+                }
+            }
+        }
+
+        stack<int> st;
+        vector<int> visited(recipes.size(), 0);
+
+        for (int i = 0; i < recipes.size(); i++) {
+            if (!visited[i]) {
+                topoSort(adjList, visited, st, i);
+            }
+        }
+
+        while (!st.empty()) {
+            int top = st.top();
+            st.pop();
+            bool canBeMade = true;
+
+            for (const string& ingredient : ingredients[top]) {
+                if (!isAvailable[ingredient]) {
+                    canBeMade = false;
+                    break;
+                }
+            }
+
+            if (canBeMade) {
+                isAvailable[recipes[top]] = true;
+                ans.push_back(recipes[top]);
+            }
+        }
+        return ans;
+    }
+};
