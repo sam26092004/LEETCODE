a06504590938c0ae0f8e7904d5235d68706231ba
sam26092004 <231158@iiitt.ac.in>
2025-04-03 17:10:49 +0530

    Time: 0 ms (100%), Space: 9.1 MB (5.88%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string str;
+
+    int numbersWithUniqueDigits(int idx, int bitmask, int tight, vector<vector<vector<int>>>& dp) {
+        if (idx == str.size())
+            return 1; 
+        
+        if (dp[idx][tight][bitmask] != -1)
+            return dp[idx][tight][bitmask];
+
+        int limit = (tight) ? str[idx] - '0' : 9;
+        int count = 0;
+
+        for (int digit = 0; digit <= limit; digit++) {
+            if (bitmask == 0 && digit == 0)
+                count += numbersWithUniqueDigits(idx + 1, bitmask, tight && (digit == limit), dp);
+            else if ((bitmask & (1 << digit)) == 0) {
+                count += numbersWithUniqueDigits(idx + 1, bitmask | (1 << digit), tight && (digit == limit), dp);
+            }
+        }
+
+        return dp[idx][tight][bitmask] = count;
+    }
+
+    int countNumbersWithUniqueDigits(int n) {
+        int maxNum = pow(10, n) - 1;
+        str = to_string(maxNum);
+
+        vector<vector<vector<int>>> dp(str.size(), vector<vector<int>>(2, vector<int>(1024, -1)));
+        return numbersWithUniqueDigits(0, 0, 1, dp);
+    }
+};
