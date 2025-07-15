8dbf22dccf60066d5768d2f2bd9f4d442ef0f89f
Satyam Rathore <231158@iiitt.ac.in>
2025-07-15 17:40:55 +0530

    Time: 1143 ms (11.6%), Space: 535.9 MB (5.56%) - LeetHub

---
+++ 3535-find-the-count-of-monotonic-pairs-i/3535-find-the-count-of-monotonic-pairs-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+    int maxNum; 
+    vector<vector<vector<int>>> dp;
+
+    int generateArrays(vector<int>& nums, int idx, int prev1, int prev2) {
+        if (idx == nums.size()) return 1;
+
+       
+        int p2 = (prev2 == INT_MAX) ? maxNum + 1 : prev2;
+
+        if (dp[idx][prev1][p2] != -1)
+            return dp[idx][prev1][p2];
+
+        int arrays = 0;
+        for (int i = 0; i <= nums[idx]; i++) {
+            if (i >= prev1 && nums[idx] - i <= prev2) {
+                arrays = (arrays + generateArrays(nums, idx + 1, i, nums[idx] - i)) % MOD;
+            }
+        }
+
+        return dp[idx][prev1][p2] = arrays;
+    }
+
+    int countOfPairs(vector<int>& nums) {
+        maxNum = *max_element(nums.begin(), nums.end());
+        int n = nums.size();
+
+        
+        dp.assign(n + 1, vector<vector<int>>(maxNum + 1, vector<int>(maxNum + 2, -1)));
+
+        return generateArrays(nums, 0, 0, INT_MAX);
+    }
+};
